7f21049b018e1ececba3b2c60f69755c1f2b89ab
ubittner <ubittner@users.noreply.github.com>
2026-01-13 08:18:14 +0100

    Added check for favorite number

--- Lighting/helper/Control.php
+++ Lighting/helper/Control.php
@@ -335,8 +335,14 @@ trait Control
 
     public function SelectFavorite(int $FavoriteNumber): void
     {
-        $this->SetValue('Favorites', $FavoriteNumber);
+        //Check if a favorite exists
         $favorites = json_decode($this->ReadPropertyString('Favorites'), true);
+        $exists = in_array($FavoriteNumber, array_column($favorites, 'FavoriteNumber'), true);
+        if (!$exists) {
+            $this->SendDebug(__FUNCTION__, $this->Translate('Favorite') . ' ' . $FavoriteNumber . ' ' . $this->Translate('does not exist!'), 0);
+            return;
+        }
+        $this->SetValue('Favorites', $FavoriteNumber);
         foreach ($favorites as $favorite) {
             if ($favorite['FavoriteNumber'] == $FavoriteNumber) {
                 if ($favorite['UseScene']) {
